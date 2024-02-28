f3dc335609e0faa72267f09d8c130fe04b23ca2d
William Inyam <sw.inyam@outlook.com>
2024-02-28 11:26:15 +0000

    build(alx_devops): add example.rb

--- 0x06-regular_expressions/example.rb
+++ 0x06-regular_expressions/example.rb
@@ -1,2 +1,15 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/127.0.0.[0-9]/).join
+#puts ARGV[0].scan(/(\w+).*\1/).join
+#mystr = "I code with java mostly, and javascript always. I know Typescript" +\
+#        "Other times, I use C++, C or Python"
+#reg_scan = mystr.scan(/(?>java(?:script)?|Python|c\b(?:\+\+)?)/i)
+mystr = "abcc"
+reg_scan = mystr.match(/a(bc|b)c/)
+
+
+puts "====== reg_scan ======"
+if reg_scan
+  puts reg_scan.to_a
+else
+  puts "no match"
+end
