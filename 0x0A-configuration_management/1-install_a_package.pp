a32828b33e92c781732b990ca5ce4317ac2592b5
William Inyam <sw.inyam@outlook.com>
2024-04-20 20:03:42 +0000

    build(asd:0x0A): add task 1 solution

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,23 @@
+# installs flask on machine using pip3
+
+package{'python3':
+  ensure  => installed,
+}
+
+package{'pip3':
+  ensure => installed,
+}
+
+exec{'install_flask':
+  command => 'pip3 install flask==2.1.0',
+  path    => ['/usr/bin', '/bin'],
+  unless  => 'pip3 show flask',
+}
+
+
+#exec{'test':
+#  command   => 'echo my_test_command',
+#  path      => ['/usr/bin/', '/bin'],
+#  onlyif    => 'ls . | grep "README.md" | wc -l',
+#  logoutput => true,
+#}
