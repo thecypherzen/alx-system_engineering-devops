8c05ea9d335d72eae1931c90cd4a94f2f56f1c04
William Inyam <sw.inyam@outlook.com>
2024-04-21 20:30:11 +0000

    style(asd:0x0A): pylintyfy task 1 file

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -7,19 +7,19 @@ package{'python3':
 
 $packages = ['jinja2','werkzeug','itsdangerous','importlib-metadata','click', 'flask']
 each($packages) | $pkg | {
-   exec{"pip3_remove $pkg":
-	  command => "pip3 uninstall --yes $pkg",
-	  path    => ['/bin/usr', '/bin'],
-	  onlyif  => "pip3 show $pkg",
-   }
-   exec{"apt_remove $pkg":
-      command => "apt-get remove python3-$pkg",
-	  path    => ['/bin/usr', '/bin'],
-	  onlyif  => "pip3 show $pkg",
-   }
+  exec{"pip3_remove ${pkg}":
+    command => "pip3 uninstall --yes ${pkg}",
+    path    => ['/bin/usr', '/bin'],
+    onlyif  => "pip3 show ${pkg}",
+  }
+  exec{"apt_remove ${pkg}":
+    command => "apt-get remove python3-${pkg}",
+    path    => ['/bin/usr', '/bin'],
+    onlyif  => "pip3 show ${pkg}",
+  }
 }
 
 exec{'install_flask':
-   command => 'pip3 install flask==2.1.0 werkzeug==2.1.1',
-   path    => ['/usr/bin', '/bin'],
+  command => 'pip3 install flask==2.1.0 werkzeug==2.1.1',
+  path    => ['/usr/bin', '/bin'],
 }
