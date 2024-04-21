014f41f497888d9050962eb71761bb726d6763d6
William Inyam <sw.inyam@outlook.com>
2024-04-21 20:24:57 +0000

    fix(asd:0x0A): resolve task 1 import err

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,23 +1,25 @@
 # installs flask on machine using pip3
+# first uninstalls all dependencies if they exist already
 
 package{'python3':
   ensure  => installed,
 }
 
-package{'python3-pip':
-  ensure => installed,
+$packages = ['jinja2','werkzeug','itsdangerous','importlib-metadata','click', 'flask']
+each($packages) | $pkg | {
+   exec{"pip3_remove $pkg":
+	  command => "pip3 uninstall --yes $pkg",
+	  path    => ['/bin/usr', '/bin'],
+	  onlyif  => "pip3 show $pkg",
+   }
+   exec{"apt_remove $pkg":
+      command => "apt-get remove python3-$pkg",
+	  path    => ['/bin/usr', '/bin'],
+	  onlyif  => "pip3 show $pkg",
+   }
 }
 
 exec{'install_flask':
-  command => 'pip3 install flask==2.1.0',
-  path    => ['/usr/bin', '/bin'],
-  unless  => 'pip3 show flask',
+   command => 'pip3 install flask==2.1.0 werkzeug==2.1.1',
+   path    => ['/usr/bin', '/bin'],
 }
-
-
-#exec{'test':
-#  command   => 'echo my_test_command',
-#  path      => ['/usr/bin/', '/bin'],
-#  onlyif    => 'ls . | grep "README.md" | wc -l',
-#  logoutput => true,
-#}
