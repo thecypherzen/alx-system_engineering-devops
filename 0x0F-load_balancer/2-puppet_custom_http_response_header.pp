2b6d032892e5d2f0815f934c7d1165710fea63c9
William Inyam <sw.inyam@outlook.com>
2024-05-05 20:59:50 +0000

    style(asd:0x0F:2): ensure puppet-lint styling

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,33 +1,33 @@
 # Configures Nginx so that its HTTP response contains a custom header
 
 exec {'update_apt':
-     command => "/usr/bin/apt update"
+  command => '/usr/bin/apt update'
 }
 
 package {'nginx':
-	ensure   => latest,
-	provider => apt,
+  ensure   => latest,
+  provider => apt,
 }
 
 
 file {'nginx_content':
-     ensure  => present,
-     path    => "/var/www/html/index.html",
-     content => "Hello World!",
+  ensure  => present,
+  path    => '/var/www/html/index.html',
+  content => 'Hello World!',
 }
 
 
 file_line {'nginx_default':
-	ensure  => present,
-	path    => "/etc/nginx/sites-available/default",
-	line    => "\t*add_header X-Served-By ${hostname};",
-	after   => '\t*try_files \$uri \$uri/ =404;',
-	require => Package['nginx'],
+  ensure  => present,
+  path    => '/etc/nginx/sites-available/default',
+  line    => "\t*add_header X-Served-By ${hostname};",
+  after   => '\t*try_files \$uri \$uri/ =404;',
+  require => Package['nginx'],
 }
 
 
 service {'nginx.service':
-	ensure  => running,
-	enable  => true,
-	require => Package['nginx'],
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx'],
 }
