7505b371ae3b4f463a4ddad05deca18427b6ad28
William Inyam <sw.inyam@outlook.com>
2024-05-05 22:03:45 +0000

    fix(asd:0x0F:2): fix here docs issue

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,15 +1,40 @@
 # Configures Nginx so that its HTTP response contains a custom header
 
+$default_config = @(EOT)
+server {
+       listen 80 default_server;
+       listen [::]:80 default_server;
+
+       root /var/www/html;
+       index index.html index.htm index.nginx-debian.html;
+
+       server_name _;
+       location / {
+                try_files $uri $uri/ =404;
+		add_header X-Served-By $hostname;
+       }
+}
+| - EOT
+
+# update apt
 exec {'update_apt':
-  command => '/usr/bin/apt update'
+  command => '/usr/bin/apt update',
 }
 
+# ensure nginx is installed
 package {'nginx':
   ensure   => latest,
   provider => apt,
 }
 
+# start nginx
+service {'nginx.service':
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx'],
+}
 
+# ensure nginx default index has right content
 file {'nginx_content':
   ensure  => present,
   path    => '/var/www/html/index.html',
@@ -17,17 +42,14 @@ file {'nginx_content':
 }
 
 
-file_line {'nginx_default':
+file {'nginx_default':
   ensure  => present,
   path    => '/etc/nginx/sites-available/default',
-  line    => "\t*add_header X-Served-By ${hostname};",
-  after   => '\t*try_files \$uri \$uri/ =404;',
-  require => Package['nginx'],
+  content => $default_config,
 }
 
-
-service {'nginx.service':
-  ensure  => running,
-  enable  => true,
-  require => Package['nginx'],
+# restart nginx
+exec {'restart_nginx':
+  command => 'systemctl restart nginx',
+  path    => ['/usr/bin', '/bin'],
 }
