6fd6cb30d960919a38ca34ea36987e011c2fc5c5
William Inyam <sw.inyam@outlook.com>
2024-05-21 04:56:04 +0000

    build(asd:0x15): add new task files

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+""" A script that uses the jsonplaceholder REST API, for a given
+employee ID, to return information about a TODO list progress and
+exports the data to json
+
+Requirements:
+    - 'urllib' or 'requests' module must be used
+    - the script must accept an integer as a parameter, which is the
+      + employee ID
+    - the script must display on the standard output the employee TODO list
+      progress in this exact format:
+
+      { "USER_ID": [{"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS,\
+      "username": "USERNAME"}, {"task": "TASK_TITLE", "completed":\
+      TASK_COMPLETED_STATUS, "username": "USERNAME"}, ... ]}
+
+      where:
+          EMPLOYEE_NAME: name of the employee
+          NUMBER_OF_DONE_TASKS: number of completed tasks
+          TOTAL_NUMBER_OF_TASKS: total number of tasks, which is the sum of
+          + completed and non-completed tasks
+      Second and N next lines display the title of completed tasks:
+      + TASK_TITLE (with 1 tabulation and 1 space before the TASK_TITLE)
+"""
+
+if __name__ == "__main__":
+    import json
+    import requests
+    import sys
+
+    if len(sys.argv) > 1:
+        id = int(sys.argv[1])
+        if id < 1 or id > 10:
+            exit()
+        uri = f"https://jsonplaceholder.typicode.com/users/{id}"
+        completed = 0
+        total = 0
+        tasks = tuple()
+
+        # get user's name and todos
+        user = dict(requests.get(url=uri).json())
+        name = user.get("name", "None")
+        todos = requests.get(url=f"{uri}/todos")
+
+        # calculate completed
+        for item in list(todos.json()):
+            tasks = tasks + (
+                ("task", item.get("title")),
+                ("completed", item.get("completed")),
+                ("username", us.get("username"))
+            )
+        msg = {f"{id}":[dict(tasks)]}
+        print(msg)
--- 0x15-api/test.py
+++ 0x15-api/test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 import requests
 
-mys = [1, 2, 3]
-print(len(mys))
+mdict = {1:'a'}
+print(mdict)
