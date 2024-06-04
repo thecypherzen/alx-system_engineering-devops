eb99db7170df155ab5dc138728b15c507c28a86b
William Inyam <sw.inyam@outlook.com>
2024-06-04 16:12:09 +0000

    build(asd:0x16:1): add task solution file

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" A script that defines a function """
+
+
+def top_ten(subreddit):
+    """ A function that queries the Reddit API and and prints the titles
+        of the first 10 hot posts listed for a given subreddit.
+
+        Requirements:
+        - If not a valid subreddit, print None.
+        - NOTE: Invalid subreddits may return a redirect to search results.
+                Ensure that you are not following redirects.
+    """
+    import requests
+    import json
+
+    hdrs = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+        "Accept": "application/json"
+    }
+    limit = 10
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json" + \
+        f"?limit={limit}&raw_json=1"
+    print(url)
+    response = requests.get(url, headers=hdrs,
+                            allow_redirects=False)
+    if response.status_code == 200:
+        objs = response.json()["data"]["children"]
+        while len(objs) > limit:
+            objs.pop()
+        for obj in objs:
+            print(obj["data"]["title"])
+    else:
+        return print("None")
