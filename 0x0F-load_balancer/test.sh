1a466d536beb06c2380daf1d1c17b6cce7324f70
William Inyam <sw.inyam@outlook.com>
2024-07-22 05:31:47 +0000

    fix(asd:0x0F): attempt checker fail fix

--- 0x0F-load_balancer/test.sh
+++ 0x0F-load_balancer/test.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-fs="$(command -v haproxy)"
-if [[ -z "$fs" ]]; then
-    echo "len 0"
-fi
+
+#fs="$(command -v haproxy)"
+#if [[ -z "$fs" ]]; then
+#    echo "len 0"
+#fi
+
+
+sed -Ei "/newline$/a $(echo -e '\ta line with tab')" myfile
